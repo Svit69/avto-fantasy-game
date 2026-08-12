6ebf89319bbb422b1e981c5930b5196e12d8660e
Svit69 <ol.svitenkov@gmail.com>
2026-08-12 19:49:42 +0500

    Fit roster screen to mobile viewport

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,16 @@ import "./styles/metrics.css";
 import "./styles/navigation.css";
 import "./styles/months.css";
 import "./styles/lineup.css";
+import "./styles/team-header.css";
 import "./styles/position.css";
 import "./styles/position-groups.css";
 import "./styles/slot.css";
 import "./styles/slot-content.css";
 import "./styles/summary.css";
+import "./styles/save-button.css";
 import "./styles/bottom-nav.css";
 import "./styles/responsive.css";
+import "./styles/compact-mobile.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
