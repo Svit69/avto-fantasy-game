173289d56ffa6d4677c96d1e594f9e4d24363b4f
Svit69 <ol.svitenkov@gmail.com>
2026-08-27 09:13:39 +0500

    Refine player card text styling

--- src/views/PlayerCardView.js
+++ src/views/PlayerCardView.js
@@ -5,19 +5,16 @@ export class PlayerCardView {
 
     return `
       <article class="player-card ${tiltClass}${selectedClass}">
-        <img class="card-layer card-bottom-layer"
-          src="/assets/card_bottom_layer.png" alt="" />
+        <img class="card-layer card-bottom-layer" src="/assets/card_bottom_layer.png" alt="" />
         <div class="card-live-layer">
           <img class="card-team-logo" src="${props.teamLogo}" alt="${props.team}" />
           <div class="card-points">${props.points}</div>
-          <img class="card-player-image" src="${props.image}"
-            alt="${props.name} ${props.secondName}" onerror="this.remove()" />
+          <img class="card-player-image" src="${props.image}" alt="${props.name} ${props.secondName}" onerror="this.remove()" />
           <div class="card-position">${this.#formatPosition(props.position)}</div>
         </div>
-        <img class="card-layer card-top-layer"
-          src="/assets/card_top_layer.png" alt="" />
+        <img class="card-layer card-top-layer" src="/assets/card_top_layer.png" alt="" />
         <div class="card-selected-label">ВЫБРАН</div>
-        <div class="card-name">${props.name.charAt(0)}. ${props.secondName}</div>
+        <div class="card-name">${this.#formatShortName(props)}</div>
         <div class="card-price">${props.price}M</div>
       </article>
     `;
@@ -26,6 +23,10 @@ export class PlayerCardView {
   #formatPosition(position) {
     const codes = { нападающий: "НАП", защитник: "ЗАЩ", вратарь: "ВРТ" };
     return codes[position] ?? position;
+  }
+
+  #formatShortName(props) {
+    return `${props.name.charAt(0)}. ${props.secondName}`.toUpperCase();
   }
 
   #selectTiltClass(orderIndex) {
