fd4037a9c6ba76fcdf573c6fd40c7e9959dc68a6
Svit69 <ol.svitenkov@gmail.com>
2026-08-27 10:50:36 +0500

    Add Golyshev player photo

--- src/data/players.js
+++ src/data/players.js
@@ -7,26 +7,26 @@ export const INITIAL_PLAYERS = Object.freeze([
   {
     firstName: "Анатолий", lastName: "Голышев", number: 11,
     position: ROSTER_POSITIONS.forward, price: 6, points: 0, team, teamLogo,
-    image: "./assets/players/anatoly_golyshev.png",
+    image: "/assets/players/anatoly_golyshev.png",
   },
   {
     firstName: "Даниэль", lastName: "Спронг", number: 91,
     position: ROSTER_POSITIONS.forward, price: 11, points: 0, team, teamLogo,
-    image: "./assets/players/daniel_sprong.png",
+    image: "/assets/players/daniel_sprong.png",
   },
   {
     firstName: "Никита", lastName: "Трямкин", number: 88,
     position: ROSTER_POSITIONS.defender, price: 7, points: 0, team, teamLogo,
-    image: "./assets/players/nikita_tryamkin.png",
+    image: "/assets/players/nikita_tryamkin.png",
   },
   {
     firstName: "Джесси", lastName: "Блэкер", number: 9,
     position: ROSTER_POSITIONS.defender, price: 8, points: 0, team, teamLogo,
-    image: "./assets/players/jesse_blacker.png",
+    image: "/assets/players/jesse_blacker.png",
   },
   {
     firstName: "Евгений", lastName: "Аликин", number: 18,
     position: ROSTER_POSITIONS.goalkeeper, price: 10, points: 0, team, teamLogo,
-    image: "./assets/players/evgeny_alikin.png",
+    image: "/assets/players/evgeny_alikin.png",
   },
 ]);
