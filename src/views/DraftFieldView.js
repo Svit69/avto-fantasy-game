66429c24f0ca2f9c4a7b0408d209f13e5a885a35
Svit69 <ol.svitenkov@gmail.com>
2026-08-27 14:26:52 +0500

    Apply custom draft typography

--- src/views/DraftFieldView.js
+++ src/views/DraftFieldView.js
@@ -8,7 +8,7 @@ export class DraftFieldView {
       <main class="scroll-area">
         <section class="draft-page">
           <div class="title-block">
-            <h1 class="draft-title">ФЭНТЕЗИ ДРАФТ</h1>
+            <h1 class="draft-title">ФЭНТЕЗИ СОСТАВ</h1>
             <p class="draft-subtitle">СОБЕРИ СОСТАВ</p>
           </div>
           <div class="tactical-board">${this.draftLineupView.render(teamRoster)}</div>
